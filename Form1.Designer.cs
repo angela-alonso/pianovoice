eb37d3738b96b3d3946cfdf7c26a1a0f8771cf8b
angela <apean8@alumni.uv.es>
2024-11-18 10:28:12 +0100

    Add files in local

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace PianoVoice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.notaDo = new System.Windows.Forms.Button();
+            this.notaRe = new System.Windows.Forms.Button();
+            this.notaMi = new System.Windows.Forms.Button();
+            this.notaFa = new System.Windows.Forms.Button();
+            this.notaSol = new System.Windows.Forms.Button();
+            this.notaLa = new System.Windows.Forms.Button();
+            this.notaSi = new System.Windows.Forms.Button();
+            this.notaDoSost = new System.Windows.Forms.Button();
+            this.notaReSost = new System.Windows.Forms.Button();
+            this.notaFaSost = new System.Windows.Forms.Button();
+            this.notaSolSost = new System.Windows.Forms.Button();
+            this.notaSiBem = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.notaDo2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // notaDo
+            // 
+            this.notaDo.BackColor = System.Drawing.Color.Violet;
+            this.notaDo.Location = new System.Drawing.Point(279, 358);
+            this.notaDo.Name = "notaDo";
+            this.notaDo.Size = new System.Drawing.Size(29, 75);
+            this.notaDo.TabIndex = 1;
+            this.notaDo.UseVisualStyleBackColor = false;
+            this.notaDo.Visible = false;
+            // 
+            // notaRe
+            // 
+            this.notaRe.BackColor = System.Drawing.Color.Violet;
+            this.notaRe.Location = new System.Drawing.Point(347, 357);
+            this.notaRe.Name = "notaRe";
+            this.notaRe.Size = new System.Drawing.Size(29, 75);
+            this.notaRe.TabIndex = 2;
+            this.notaRe.UseVisualStyleBackColor = false;
+            this.notaRe.Visible = false;
+            // 
+            // notaMi
+            // 
+            this.notaMi.BackColor = System.Drawing.Color.Violet;
+            this.notaMi.Location = new System.Drawing.Point(413, 357);
+            this.notaMi.Name = "notaMi";
+            this.notaMi.Size = new System.Drawing.Size(29, 75);
+            this.notaMi.TabIndex = 3;
+            this.notaMi.UseVisualStyleBackColor = false;
+            this.notaMi.Visible = false;
+            // 
+            // notaFa
+            // 
+            this.notaFa.BackColor = System.Drawing.Color.Violet;
+            this.notaFa.Location = new System.Drawing.Point(480, 358);
+            this.notaFa.Name = "notaFa";
+            this.notaFa.Size = new System.Drawing.Size(29, 75);
+            this.notaFa.TabIndex = 4;
+            this.notaFa.UseVisualStyleBackColor = false;
+            this.notaFa.Visible = false;
+            // 
+            // notaSol
+            // 
+            this.notaSol.BackColor = System.Drawing.Color.Violet;
+            this.notaSol.Location = new System.Drawing.Point(545, 358);
+            this.notaSol.Name = "notaSol";
+            this.notaSol.Size = new System.Drawing.Size(29, 75);
+            this.notaSol.TabIndex = 5;
+            this.notaSol.UseVisualStyleBackColor = false;
+            this.notaSol.Visible = false;
+            // 
+            // notaLa
+            // 
+            this.notaLa.BackColor = System.Drawing.Color.Violet;
+            this.notaLa.Location = new System.Drawing.Point(612, 357);
+            this.notaLa.Name = "notaLa";
+            this.notaLa.Size = new System.Drawing.Size(29, 75);
+            this.notaLa.TabIndex = 6;
+            this.notaLa.UseVisualStyleBackColor = false;
+            this.notaLa.Visible = false;
+            // 
+            // notaSi
+            // 
+            this.notaSi.BackColor = System.Drawing.Color.Violet;
+            this.notaSi.Location = new System.Drawing.Point(675, 357);
+            this.notaSi.Name = "notaSi";
+            this.notaSi.Size = new System.Drawing.Size(29, 75);
+            this.notaSi.TabIndex = 7;
+            this.notaSi.UseVisualStyleBackColor = false;
+            this.notaSi.Visible = false;
+            // 
+            // notaDoSost
+            // 
+            this.notaDoSost.BackColor = System.Drawing.Color.Aquamarine;
+            this.notaDoSost.Location = new System.Drawing.Point(333, 189);
+            this.notaDoSost.Name = "notaDoSost";
+            this.notaDoSost.Size = new System.Drawing.Size(11, 81);
+            this.notaDoSost.TabIndex = 8;
+            this.notaDoSost.UseVisualStyleBackColor = false;
+            this.notaDoSost.Visible = false;
+            // 
+            // notaReSost
+            // 
+            this.notaReSost.BackColor = System.Drawing.Color.Aquamarine;
+            this.notaReSost.Location = new System.Drawing.Point(395, 189);
+            this.notaReSost.Name = "notaReSost";
+            this.notaReSost.Size = new System.Drawing.Size(11, 81);
+            this.notaReSost.TabIndex = 9;
+            this.notaReSost.UseVisualStyleBackColor = false;
+            this.notaReSost.Visible = false;
+            // 
+            // notaFaSost
+            // 
+            this.notaFaSost.BackColor = System.Drawing.Color.Aquamarine;
+            this.notaFaSost.Location = new System.Drawing.Point(484, 189);
+            this.notaFaSost.Name = "notaFaSost";
+            this.notaFaSost.Size = new System.Drawing.Size(11, 81);
+            this.notaFaSost.TabIndex = 10;
+            this.notaFaSost.UseVisualStyleBackColor = false;
+            this.notaFaSost.Visible = false;
+            // 
+            // notaSolSost
+            // 
+            this.notaSolSost.BackColor = System.Drawing.Color.Aquamarine;
+            this.notaSolSost.Location = new System.Drawing.Point(244, 189);
+            this.notaSolSost.Name = "notaSolSost";
+            this.notaSolSost.Size = new System.Drawing.Size(11, 81);
+            this.notaSolSost.TabIndex = 11;
+            this.notaSolSost.UseVisualStyleBackColor = false;
+            this.notaSolSost.Visible = false;
+            // 
+            // notaSiBem
+            // 
+            this.notaSiBem.BackColor = System.Drawing.Color.Aquamarine;
+            this.notaSiBem.Location = new System.Drawing.Point(542, 189);
+            this.notaSiBem.Name = "notaSiBem";
+            this.notaSiBem.Size = new System.Drawing.Size(11, 81);
+            this.notaSiBem.TabIndex = 12;
+            this.notaSiBem.UseVisualStyleBackColor = false;
+            this.notaSiBem.Visible = false;
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.Aquamarine;
+            this.button13.Location = new System.Drawing.Point(600, 189);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(11, 81);
+            this.button13.TabIndex = 13;
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Visible = false;
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.Violet;
+            this.button14.Location = new System.Drawing.Point(213, 358);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(29, 75);
+            this.button14.TabIndex = 14;
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Visible = false;
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.Violet;
+            this.button15.Location = new System.Drawing.Point(147, 357);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(29, 75);
+            this.button15.TabIndex = 15;
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Visible = false;
+            // 
+            // notaDo2
+            // 
+            this.notaDo2.BackColor = System.Drawing.Color.Violet;
+            this.notaDo2.Location = new System.Drawing.Point(733, 357);
+            this.notaDo2.Name = "notaDo2";
+            this.notaDo2.Size = new System.Drawing.Size(29, 75);
+            this.notaDo2.TabIndex = 16;
+            this.notaDo2.UseVisualStyleBackColor = false;
+            this.notaDo2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.notaDo2);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.notaSiBem);
+            this.Controls.Add(this.notaSolSost);
+            this.Controls.Add(this.notaFaSost);
+            this.Controls.Add(this.notaReSost);
+            this.Controls.Add(this.notaDoSost);
+            this.Controls.Add(this.notaSi);
+            this.Controls.Add(this.notaLa);
+            this.Controls.Add(this.notaSol);
+            this.Controls.Add(this.notaFa);
+            this.Controls.Add(this.notaMi);
+            this.Controls.Add(this.notaRe);
+            this.Controls.Add(this.notaDo);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "PianoVoice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button notaDo;
+        private System.Windows.Forms.Button notaRe;
+        private System.Windows.Forms.Button notaMi;
+        private System.Windows.Forms.Button notaFa;
+        private System.Windows.Forms.Button notaSol;
+        private System.Windows.Forms.Button notaLa;
+        private System.Windows.Forms.Button notaSi;
+        private System.Windows.Forms.Button notaDoSost;
+        private System.Windows.Forms.Button notaReSost;
+        private System.Windows.Forms.Button notaFaSost;
+        private System.Windows.Forms.Button notaSolSost;
+        private System.Windows.Forms.Button notaSiBem;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button notaDo2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Media;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+
+namespace PianoVoice
+{
+    public partial class Form1 : Form
+    {
+        private System.Speech.Recognition.SpeechRecognitionEngine _recognizer =
+        new SpeechRecognitionEngine();
+        private SpeechSynthesizer synth = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            synth.Speak("Bienvenido al diseño de interfaces avanzadas. Inicializando la Aplicación");
+
+            Grammar grammar = CreateGrammarBuilderMusic(null);
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.UnloadAllGrammars();
+
+            // Nivel de confianza del reconocimiento 70%
+            _recognizer.UpdateRecognizerSetting("CFGConfidenceRejectionThreshold", 50);
+
+            grammar.Enabled = true;
+            _recognizer.LoadGrammar(grammar);
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(_recognizer_SpeechRecognized);
+
+            //reconocimiento asíncrono y múltiples veces
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            synth.Speak("Aplicación preparada para reconocer su voz");
+        }
+
+        void _recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //obtenemos un diccionario con los elementos semánticos
+            SemanticValue semantics = e.Result.Semantics;
+            RecognitionResult result = e.Result;
+            SoundPlayer player;
+
+            if (!semantics.ContainsKey("escalaMusical"))
+            {
+                synth.Speak("No info provided");
+            }
+            else
+            {
+                string notaReconocida = semantics["escalaMusical"].Value.ToString().ToLower(); // Obtener el valor semántico
+                string basePath = AppDomain.CurrentDomain.BaseDirectory;
+                string soundFile = basePath;
+
+
+                // Reproducir la nota basada en la entrada de voz
+                switch (notaReconocida)
+                {
+                    case "do":
+                        soundFile = Path.Combine(basePath, "Music", "do.wav");
+                        this.notaDo.Visible = true;
+                        break;
+                    case "re":
+                        soundFile = Path.Combine(basePath, "Music", "re.wav");
+                        this.notaRe.Visible = true;
+                        break;
+                    case "mi":
+                        soundFile = Path.Combine(basePath, "Music", "mi.wav");
+                        this.notaMi.Visible = true;
+                        break;
+                    case "fa":
+                        soundFile = Path.Combine(basePath, "Music", "fa.wav");
+                        this.notaFa.Visible = true;
+                        break;
+                    case "sol":
+                        soundFile = Path.Combine(basePath, "Music", "sol.wav");
+                        this.notaSol.Visible = true;
+                        break;
+                    case "la":
+                        soundFile = Path.Combine(basePath, "Music", "la.wav");
+                        this.notaLa.Visible = true;
+                        break;
+                    case "si":
+                        soundFile = Path.Combine(basePath, "Music", "si.wav");
+                        this.notaSi.Visible = true;
+                        break;
+                }
+
+                player = new SoundPlayer(soundFile);
+                player.Play();
+
+                Timer timer = new Timer();
+                timer.Interval = 1500; // Duración del sonido en milisegundos
+                timer.Tick += (s, args) =>
+                {
+                    this.Controls.OfType<Button>().ToList().ForEach(b => b.Visible = false);
+                    timer.Stop(); // Detener el temporizador
+                };
+                timer.Start();
+
+                Update();
+            }
+        }
+
+        private Grammar CreateGrammarBuilderMusic(params int[] info)
+        {
+            //synth.Speak("Creando ahora la gramática");
+            Choices notaChoice = new Choices();
+
+            SemanticResultValue choiceResultValue;
+            GrammarBuilder resultValueBuilder;
+
+            choiceResultValue = new SemanticResultValue("Do", "do");
+            resultValueBuilder = new GrammarBuilder(choiceResultValue);
+            notaChoice.Add(resultValueBuilder);
+
+            choiceResultValue = new SemanticResultValue("Re", "re");
+            resultValueBuilder = new GrammarBuilder(choiceResultValue);
+            notaChoice.Add(resultValueBuilder);
+
+            choiceResultValue = new SemanticResultValue("Mi", "mi");
+            resultValueBuilder = new GrammarBuilder(choiceResultValue);
+            notaChoice.Add(resultValueBuilder);
+
+            choiceResultValue = new SemanticResultValue("Fa", "fa");
+            resultValueBuilder = new GrammarBuilder(choiceResultValue);
+            notaChoice.Add(resultValueBuilder);
+
+            choiceResultValue = new SemanticResultValue("Sol", "sol");
+            resultValueBuilder = new GrammarBuilder(choiceResultValue);
+            notaChoice.Add(resultValueBuilder);
+
+            choiceResultValue = new SemanticResultValue("La", "la");
+            resultValueBuilder = new GrammarBuilder(choiceResultValue);
+            notaChoice.Add(resultValueBuilder);
+
+            choiceResultValue = new SemanticResultValue("Si", "si");
+            resultValueBuilder = new GrammarBuilder(choiceResultValue);
+            notaChoice.Add(resultValueBuilder);
+
+            SemanticResultKey choiceResultKey = new SemanticResultKey("escalaMusical", notaChoice);
+            GrammarBuilder notas = new GrammarBuilder(choiceResultKey);
+
+            GrammarBuilder tocar = "Tocar";
+            GrammarBuilder nota = "Nota";
+            //GrammarBuilder acorde = "Acorde";
+            //GrammarBuilder escala = "Escala";
+
+            GrammarBuilder frase = tocar;
+            frase.Append(notas);
+
+            Grammar grammar = new Grammar(frase);
+            grammar.Name = "Tocar Piano";
+
+            return grammar;
+        }
+    }
+}
